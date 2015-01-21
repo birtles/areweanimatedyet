0e20badca75acfa8dc928684b992f65df227e732
Brian Birtles <birtles@gmail.com>
2015-01-21 14:50:07 +0900

    Refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    source = require('vinyl-source-stream'),
+    transform = require('vinyl-transform');
 
 gulp.task('browserify', function() {
   var browserify = require('browserify'),
-      reactify = require('reactify'),
-      buffer = require('vinyl-buffer'),
-      source = require('vinyl-source-stream'),
-      transform = require('vinyl-transform'),
-      uglify = require('gulp-uglify'),
-      sourcemaps = require('gulp-sourcemaps'),
-      concat = require('gulp-concat');
+      reactify = require('reactify');
 
   return browserify('./src/main.jsx')
     .transform(reactify)
     .bundle()
-    .pipe(source('areweanimatedyet.js'))
-    .pipe(buffer())
+    .pipe(source('areweanimatedyet.dev.js'))
+    .pipe(gulp.dest('.'))
+    ;
+});
+
+gulp.task('minify', ['browserify'], function() {
+  var buffer = require('vinyl-buffer'),
+      uglify = require('gulp-uglify'),
+      sourcemaps = require('gulp-sourcemaps'),
+      concat = require('gulp-concat');
+
+  return gulp.src('areweanimatedyet.dev.js')
     .pipe(sourcemaps.init())
       // This is needed to work around a bug in uglify:
       // https://github.com/terinjokes/gulp-uglify/issues/56
