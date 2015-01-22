e063c7a80fdd6c370df9f793c049536f73204868
Brian Birtles <birtles@gmail.com>
2015-01-22 14:11:04 +0900

    Debug deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -70,10 +70,13 @@ gulp.task('watch', function() {
 gulp.task('deploy', function() {
   var deploy = require('gulp-gh-pages');
 
+  console.log(process.env.TRAVIS_REPO_SLUG);
+  console.log(process.env.GH_TOKEN ? "GH_TOKEN is set" : "GH_TOKEN not set!");
   var repo = process.env.GH_TOKEN && process.env.TRAVIS_REPO_SLUG
-           ?  'https://' + process.env.GH_TOKEN + '@github.com/'
-                + process.env.TRAVIS_REPO_SLUG + '.git'
+           ? 'https://' + process.env.GH_TOKEN + '@github.com/'
+               + process.env.TRAVIS_REPO_SLUG + '.git'
            : undefined;
+  console.log("repo.length: " + repo.length);
 
   return gulp.src(
     [ 'index.html',
